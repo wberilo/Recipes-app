4b6b347c07f5fa107c63f8e1f153aac7652433c3
wberilo <wberilo@gmail.com>
2021-01-26 19:31:29 -0300

    req 27

--- src/pages/Bebidas.jsx
+++ src/pages/Bebidas.jsx
@@ -2,20 +2,36 @@ import React, { useState, useEffect } from 'react';
 
 function Bebidas() {
   const [beverageCards, setBeverageCards] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     async function grabFoodItems() {
       const fetched = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=')
         .then((data) => data.json());
+      const fetchCategories = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
+        .then((data) => data.json());
       const min = 0;
-      const max = 12;
-      setBeverageCards(fetched.drinks.slice(min, max));
+      const max = 5;
+      const max2 = 12;
+      setCategories(fetchCategories.drinks.slice(min, max));
+      setBeverageCards(fetched.drinks.slice(min, max2));
     }
     grabFoodItems();
   }, []);
 
   return (
     <div>
+      <div>
+        {categories.map((card, index) => (
+          <button
+            data-testid={ `${card.strCategory}-category-filter` }
+            key={ index }
+            type="button"
+          >
+            {card.strCategory}
+          </button>
+        ))}
+      </div>
       {beverageCards.map((card, index) => (
         <div
           key={ index }
--- src/pages/Comidas.jsx
+++ src/pages/Comidas.jsx
@@ -2,22 +2,36 @@ import React, { useEffect, useState } from 'react';
 
 function Comidas() {
   const [foodCards, setFoodCards] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     async function grabFoodItems() {
       const fetched = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
         .then((data) => data.json());
-      console.log(fetched.meals);
+      const fetchCategories = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list')
+        .then((data) => data.json());
       const min = 0;
-      const max = 12;
-      setFoodCards(fetched.meals.slice(min, max));
+      const max = 5;
+      const max2 = 12;
+      setCategories(fetchCategories.meals.slice(min, max));
+      setFoodCards(fetched.meals.slice(min, max2));
     }
     grabFoodItems();
   }, []);
 
   return (
     <div>
-      <p>Comidas</p>
+      <div>
+        {categories.map((card, index) => (
+          <button
+            data-testid={ `${card.strCategory}-category-filter` }
+            key={ index }
+            type="button"
+          >
+            {card.strCategory}
+          </button>
+        ))}
+      </div>
       {foodCards.map((card, index) => (
         <div
           key={ index }
