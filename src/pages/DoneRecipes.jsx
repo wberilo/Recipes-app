8d6f66a9d65cfcbdeed3991a167b6eb893321c35
wberilo <wberilo@gmail.com>
2021-02-02 15:12:49 -0300

    small fix

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -5,7 +5,10 @@ import { Button } from 'react-bootstrap';
 import DoneCard from './Components/DoneCard';
 
 function DoneRecipes() {
-  const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+  let doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+  if (doneRecipes === null) {
+    doneRecipes = [];
+  }
   const [shownRecipes, setShownRecipes] = useState(doneRecipes);
   return (
     <div>
